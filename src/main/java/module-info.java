e28b7e8ccf77001a6704fd4b09686e974c62fbd8
Lê Ngọc Vĩ <vile2040@gmail.com>
2025-03-29 22:25:42 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.hcmus.exammanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.hcmus.exammanagement to javafx.fxml;
+    exports com.hcmus.exammanagement;
+}
