23bc57accba3aea85caf26eef2acd5060e162ad3
Lê Ngọc Vĩ <110024393+luinor223@users.noreply.github.com>
2025-04-29 21:54:47 +0700

    Fix sql script

--- script/create_db.sql
+++ script/create_db.sql
@@ -6,7 +6,10 @@
 --     LOCALE_PROVIDER = 'libc'
 --     CONNECTION LIMIT = -1
 --     IS_TEMPLATE = False;
-\connect "ExamCenterDB" postgres;
+--\connect "ExamCenterDB" postgres;
+DROP SCHEMA IF EXISTS Nhom01 CASCADE;
+CREATE SCHEMA Nhom01;
+SET search_path TO Nhom01;
 
 CREATE SEQUENCE seq_thi_sinh START 1;
 CREATE SEQUENCE seq_khach_hang START 1;
@@ -25,7 +28,6 @@ CREATE SEQUENCE seq_chi_tiet_phieu_dk START 1;
 CREATE SEQUENCE seq_phong START 1;
 
 -- Bảng Thí Sinh
-DROP TABLE IF EXISTS thi_sinh CASCADE;
 CREATE TABLE thi_sinh (
     ma_ts TEXT PRIMARY KEY DEFAULT 'TS' || LPAD(nextval('seq_thi_sinh')::TEXT, 6, '0'),
     ho_ten VARCHAR(100) NOT NULL,
@@ -35,7 +37,6 @@ CREATE TABLE thi_sinh (
 );
 
 -- Bảng Khách Hàng
-DROP TABLE IF EXISTS khach_hang CASCADE;
 CREATE TABLE khach_hang (
     ma_kh TEXT PRIMARY KEY DEFAULT 'KH' || LPAD(nextval('seq_khach_hang')::TEXT, 6, '0'),
     ho_ten VARCHAR(100) NOT NULL,
@@ -47,7 +48,6 @@ CREATE TABLE khach_hang (
 );
 
 -- Bảng Nhân Viên
-DROP TABLE IF EXISTS nhan_vien CASCADE;
 CREATE TABLE nhan_vien (
     ma_nv TEXT PRIMARY KEY DEFAULT 'NV' || LPAD(nextval('seq_nhan_vien')::TEXT, 6, '0'),
     ho_ten VARCHAR(100) NOT NULL,
@@ -57,10 +57,8 @@ CREATE TABLE nhan_vien (
 );
 
 -- Bảng Phiếu Đăng Ký
-DROP TABLE IF EXISTS phieu_dang_ky CASCADE;
 CREATE TABLE phieu_dang_ky (
     ma_pdk TEXT PRIMARY KEY DEFAULT 'PDK' || LPAD(nextval('seq_phieu_dang_ky')::TEXT, 6, '0'),
-    han_nop DATE,
     trang_thai VARCHAR(50),
     ngay_lap DATE DEFAULT CURRENT_DATE,
     dia_chi_giao VARCHAR(100),
@@ -71,7 +69,6 @@ CREATE TABLE phieu_dang_ky (
 );
 
 -- Bảng Chứng Chỉ
-DROP TABLE IF EXISTS chung_chi CASCADE;
 CREATE TABLE chung_chi (
     ma_cchi TEXT PRIMARY KEY DEFAULT 'CC' || LPAD(nextval('seq_chung_chi')::TEXT, 6, '0'),
     ten_chung_chi VARCHAR(100),
@@ -81,7 +78,6 @@ CREATE TABLE chung_chi (
 );
 
 -- Bảng Phòng
-DROP TABLE IF EXISTS phong CASCADE;
 CREATE TABLE phong (
     ma_phong TEXT PRIMARY KEY DEFAULT 'P' || LPAD(nextval('seq_phong')::TEXT, 6, '0'),
     ten_phong VARCHAR(50) NOT NULL,
@@ -90,7 +86,6 @@ CREATE TABLE phong (
 );
 
 -- Bảng Lịch Thi
-DROP TABLE IF EXISTS lich_thi CASCADE;
 CREATE TABLE lich_thi (
     ma_lt TEXT PRIMARY KEY DEFAULT 'LT' || LPAD(nextval('seq_lich_thi')::TEXT, 6, '0'),
     ngay_gio_thi TIMESTAMP,
@@ -101,7 +96,6 @@ CREATE TABLE lich_thi (
 );
 
 -- Bảng Chi Tiết Phiếu Đăng Ký
-DROP TABLE IF EXISTS chi_tiet_phieu_dk CASCADE;
 CREATE TABLE chi_tiet_phieu_dk (
     ma_ctpdk TEXT PRIMARY KEY DEFAULT 'CTPDK' || LPAD(nextval('seq_chi_tiet_phieu_dk')::TEXT, 6, '0'),
     ma_pdk TEXT NOT NULL,
@@ -114,7 +108,6 @@ CREATE TABLE chi_tiet_phieu_dk (
 );
 
 -- Bảng Chi Tiết Phòng Thi
-DROP TABLE IF EXISTS chi_tiet_phong_thi CASCADE;
 CREATE TABLE chi_tiet_phong_thi (
     ma_ctpt TEXT PRIMARY KEY DEFAULT 'CTPT' || LPAD(nextval('seq_chi_tiet_phong_thi')::TEXT, 6, '0'),
     ma_phong TEXT NOT NULL,
@@ -128,7 +121,6 @@ CREATE TABLE chi_tiet_phong_thi (
 );
 
 -- Bảng Giám Thị
-DROP TABLE IF EXISTS giam_thi CASCADE;
 CREATE TABLE giam_thi (
     ma_gt TEXT PRIMARY KEY DEFAULT 'GT' || LPAD(nextval('seq_giam_thi')::TEXT, 6, '0'),
     ho_ten VARCHAR(100) NOT NULL,
@@ -137,7 +129,6 @@ CREATE TABLE giam_thi (
 );
 
 -- Bảng Giám Sát Thi
-DROP TABLE IF EXISTS giam_sat_thi CASCADE;
 CREATE TABLE giam_sat_thi (
     ma_lt TEXT,
     ma_gt TEXT,
@@ -148,7 +139,6 @@ CREATE TABLE giam_sat_thi (
 );
 
 -- Bảng Phiếu Dự Thi
-DROP TABLE IF EXISTS phieu_du_thi CASCADE;
 CREATE TABLE phieu_du_thi (
     ma_pdt TEXT PRIMARY KEY DEFAULT 'PDT' || LPAD(nextval('seq_phieu_du_thi')::TEXT, 6, '0'),
     sbd CHAR(6),
@@ -159,7 +149,6 @@ CREATE TABLE phieu_du_thi (
 );
 
 -- Bảng Kết Quả
-DROP TABLE IF EXISTS ket_qua CASCADE;
 CREATE TABLE ket_qua (
     ma_kq TEXT PRIMARY KEY DEFAULT 'KQ' || LPAD(nextval('seq_ket_qua')::TEXT, 6, '0'),
     ngay_gio_thi DATE,
@@ -175,7 +164,6 @@ CREATE TABLE ket_qua (
 );
 
 -- Bảng Hóa Đơn
-DROP TABLE IF EXISTS hoa_don CASCADE;
 CREATE TABLE hoa_don (
     ma_hd TEXT PRIMARY KEY DEFAULT 'HD' || LPAD(nextval('seq_hoa_don')::TEXT, 6, '0'),
     tong_tien FLOAT,
@@ -190,7 +178,6 @@ CREATE TABLE hoa_don (
 );
 
 -- Bảng Phiếu Gia Hạn
-DROP TABLE IF EXISTS phieu_gia_han CASCADE;
 CREATE TABLE phieu_gia_han (
     ma_pgh TEXT PRIMARY KEY DEFAULT 'PGH' || LPAD(nextval('seq_phieu_gia_han')::TEXT, 6, '0'),
     loai_gh VARCHAR(20),
